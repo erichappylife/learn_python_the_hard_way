99656444417ca28fcb8ae7065dbf1a517884bc10
eric_happylife <eric_happylife@163.com>
2020-01-05 00:48:36 +0800

    feat:  ex1, ex2

---
+++ ex2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignore by python
+
+print("I cloud have code like this") # and the comment after is ignored
+
+# you can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
+
+print("This is run.")
