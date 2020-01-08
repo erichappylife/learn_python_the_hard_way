5d77ee744b22cb9686a54d43d02d313ebfc13b39
eric_happylife <eric_happylife@163.com>
2020-01-08 23:39:21 +0800

    fix: modify print content

--- ex2.py
+++ ex2.py
@@ -4,7 +4,7 @@
 # A comment, this is so you can read your program later.
 # Anything after the # is ignore by python
 
-print("I cloud have code like this") # and the comment after is ignored
+print("I could have code like this") # and the comment after is ignored
 
 # you can also use a comment to "disable" or comment out a piece of code:
 # print "This won't run."
