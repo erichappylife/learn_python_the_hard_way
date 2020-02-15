0ee5f50a1c10eb2eb553867ddd6c8c281d19ae1b
eric_happylife <eric_happylife@163.com>
2020-02-15 13:03:19 +0800

    fix: ex21 modify function name

--- ex21.py
+++ ex21.py
@@ -5,8 +5,8 @@ def add(a, b):
     print(f"ADDING {a} + {b}")
     return a + b
 
-def substract(a, b):
-    print(f"SUBSTRACTING {a} - {b}")
+def subtract(a, b):
+    print(f"SUBTRACTING {a} - {b}")
     return a - b
 
 def multiply(a, b):
@@ -20,7 +20,7 @@ def divide(a, b):
 print("Let's do some math with just functions!")
 
 age = add(30, 5)
-height = substract(78, 4)
+height = subtract(78, 4)
 weight = multiply(90, 2)
 iq = divide(100, 2)
 
@@ -30,6 +30,6 @@ print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
 # A puzzle for the extra credit, type it in anyway.
 print("Here is a puzzle.")
 
-what = add(age, substract(height, multiply(weight, divide(iq, 2))))
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 print("That becomes: ", what, "Can you do it by hand?")
